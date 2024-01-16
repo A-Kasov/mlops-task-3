c34f5638b759d7f9e7e76ef1316415a4acd5bddd
A-Kasov <kasovarteom@yandex.ru>
2024-01-16 06:58:27 +0500

    del

--- scripts/test.py
+++
@@ -1,9 +0,0 @@
-import os
-import gdown
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-def download_dataset(id):
-    gdown.download(id=id, output=os.path.join(BASE_DIR, "datasets/train.csv"))
-
-download_dataset('14YPaOI3VspacNH226zhHsAegaoqn9nui')
